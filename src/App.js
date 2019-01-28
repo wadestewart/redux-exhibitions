8f0755eb4961111b42b80720dccbea45b6ff5599
Wade Stewart <stew.wade@gmail.com>
2019-01-28 10:36:28 -0500

    cleaned out starter code, added exhibitions, added fertch logic to app, starting aexhibitions card

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import { API } from './config'
+import Exhibitions from './Exhibitions'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.handleLoadExhibitions = this.handleLoadExhibitions.bind(this)
+
+    this.state = {
+      exhibits: [],
+      exhibitions: []
+    }
+  }
+
+
+  handleLoadExhibitions = (exhibition) => {
+    fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getObjects&access_token=${API.apiKey}&exhibition_id=${exhibition.id}&page=1&per_page=100`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        // this.setState({ exhibits: data.objects })
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount = () => {
+    fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getList&access_token=${API.apiKey}&page=1&per_page=100`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ exhibitions: data.exhibitions })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
+    // console.log(this.state.exhibits)
+    const exhibitions = this.state.exhibitions
+    let exhibitionsWithProps =
+      this.state.exhibitions.length !== 0 ?
+        <Exhibitions
+          exhibitions={exhibitions}
+          onLoadExhibitions={this.handleLoadExhibitions}
+        />
+      :
+        null
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {exhibitionsWithProps}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Exhibitions.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+import preloaderImage from './cooperhewitt.jpg'
+
+
+class Exhibitions extends Component {
+    constructor(props) {
+        super(props)
+        console.log(this.props.exhibitions)
+
+        this.state = {
+            images: []
+        }
+    }
+
+
+    // fetchData = () => {
+    //     fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getRandom&access_token=21313fcea367520d564f1b69b4b0b6cf&has_image=1`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data.object)
+    //     })
+    //     .catch(err => console.log(err))
+    // }
+
+    // componentDidMount = () => {
+    //     this.fetchData()
+    // }
+
+    render() {
+
+        // const exhibitions = this.props.exhibitions.map(exhibition => {
+        //     return (
+        //         <ExhibitionCard
+        //           key={exhibition.id}
+        //           id={exhibition.id}
+        //           title={exhibition.title}
+        //           text={exhibition.text}
+        //           start={exhibition.date_start}
+        //           end={exhibition.date_end}
+        //           onDetailsClick={() => this.props.onDetailsClick(exhibition)}
+        //         />
+        //     )
+        //   })
+
+        return (
+            <figure>
+                <h1>Yello Sucka</h1>
+                <img src={preloaderImage} alt="" onLoad={() => console.log('loaded')} />
+            </figure>
+        )
+    }
+}
+
+export default Exhibitions
