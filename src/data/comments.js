3db9b1eedd21b3caac0ab7183db513525c69856f
Wade Stewart <stew.wade@gmail.com>
2019-01-31 16:33:25 -0500

    reorganized data for redux store, adding artworks and comments reducers

---
+++ src/data/comments.js
@@ -0,0 +1,86 @@
+const comments = {
+    "18621831": [
+        {
+        "text":"Wes. WE should have lunch.",
+        "user": "jdaveknox"
+        },
+        {
+        "text":"#adults",
+        "user": "jdaveknox"
+        },
+        {
+        "text":"@jdaveknox yes!",
+        "user": "wesbos"
+        },
+        {
+        "text":"😍 love Hamilton!",
+        "user": "willowtreemegs"
+        }
+    ],
+    "286494701": [
+        {
+        "text":"Those are cute! They're like silver dollar pancakes.",
+        "user": "rrsimonsen"
+        },
+        {
+        "text":"I like baby pancakes but gluten free please!! I'll bring the coffee!! See you in 6 days!!!!!! 😝😛😝♥️",
+        "user": "terzisn"
+        },
+        {
+        "text":"... and apparently growing baby. 👀. Yum.",
+        "user": "henrihelvetica"
+        },
+        {
+        "text":"@wesbos 👍 my daughter is a pancake eating machine!",
+        "user": "brentoage"
+        }
+    ],
+    "1141960825": [
+        {
+        "text":"Looking great Wes! I'd like to see the other side of the room too.",
+        "user": "axcdnt"
+        },
+        {
+        "text":"I've never caught your podcast. Have one right? Btw - they don't have a Canary pillow? 😝",
+        "user": "henrihelvetica"
+        },
+        {
+        "text":"Great way to start the year.",
+        "user": "pmgllc"
+        }
+    ],
+    "1158789265": [
+        {
+        "text":"Did she get to eat her letter?",
+        "user": "pathiebert"
+        },
+        {
+        "text":"Nope @pathiebert! She has too much teeth now (8) so that would definitely be her first cavity 😉",
+        "user": "kaitbos"
+        }
+    ],
+    "1158789277": [
+        {
+        "text":"Super congrats! That's wicked cool and looks great.",
+        "user": "pmgllc"
+        },
+        {
+        "text":"real live paper magazine? woah haha. flex box is awesome though, could rage quit without it",
+        "user": "tjholowaychuk2"
+        },
+        {
+        "text":"@tjholowaychuk2 haha yes! Old school stylez!",
+        "user": "wesbos"
+        }
+    ],
+    "1158789289": [
+        {
+        "text":"😍",
+        "user": "melsariffodeen"
+        },
+        {
+        "text":"Very cool!",
+        "user": "ka11away"
+        }
+    ],
+}
---
+++ src/reducers/posts.js
@@ -0,0 +1,12 @@
+// A reducer takes in two things
+
+// 1. the action (info about what happened)
+
+// 2. Copy of current state
+
+function posts(state = [], action) {
+    console.log(state, action)
+    return state
+}
+
+export default posts
