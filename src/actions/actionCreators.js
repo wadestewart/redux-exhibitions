7328e20069d3a92b1e9bbb8a7958474e4eca14be
Wade Stewart <stew.wade@gmail.com>
2019-01-31 15:11:24 -0500

    added actions (actionCreators) to project

---
+++ src/actions/actionCreators.js
@@ -0,0 +1,27 @@
+// Increment likes
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+}
+
+// Add Comment
+
+export function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+// Remove Comment
+export function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    }
+} 
